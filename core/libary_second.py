ff6ed7dfcbe4b28715569d937ff34b55253bb61e
Shlomo <shlomo1839@gmail.com>
2025-11-10 12:51:43 +0200

    added something of files

---
+++ core/libary_second.py
@@ -0,0 +1,8 @@
+def add_user(self, user):
+        self.users.append(user)
+        print(f"{user} add to your libary")
+
+def borrow_book(user_id, book_isbn):
+#     book.is_availble = False
+#     user.borrowed_books.append()
+    pass
