9de9d4ac5dcda41bc7eb535aaade1c9ad2436f20
Shlomo <shlomo1839@gmail.com>
2025-11-11 11:36:15 +0200

    user class

--- core/user.py
+++ core/user.py
@@ -1,5 +1,7 @@
 class User:
+    UID = 2000
     def __init__(self, name: str, id: str, borrowed_books: list):
         self.name = name
-        self.id = id
+        self.id = User.UID
+        User.UID += 1
         self.borrowed_books = borrowed_books
